10ef8d9b65145ed705afb7d0e28a4cfa44d07858
QiuLi22 <104408128+QiuLi22@users.noreply.github.com>
2022-04-29 21:13:52 -0700

    Add project files.

---
+++ SimpleCalculator/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Qiu Li 4/29/2022 Extra 6-1
+
+        private decimal Calculate(decimal operand1, string operator1, decimal operand2)
+        {
+            decimal result = 0;
+            if (operator1 == "+")
+            {
+                result = operand1 + operand2;
+            }
+            else if (operator1 == "-")
+            {
+                result = operand1 - operand2;
+            }
+            else if (operator1 == "*")
+            {
+                result = operand1 * operand2;
+            }
+            else if (operator1 == "/")
+            {
+                result = operand1 / operand2;
+            }
+
+            return result;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            decimal operand1 = Convert.ToDecimal(txtOperand1.Text);
+            decimal operand2 = Convert.ToDecimal(txtOperand2.Text);
+            string operator1 = txtOperator.Text;
+
+            decimal result = Calculate(operand1, operator1, operand2);
+            result = Decimal.Round(result, 4);
+            txtResult.Text = result.ToString();
+            txtOperand1.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            txtResult.Text = "";
+        }
+    }
+}
